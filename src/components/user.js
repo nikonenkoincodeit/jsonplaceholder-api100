f8cb6ad544527a68f32953fa64a46e7040c0484c
Andromeda-NGC224 <shadim115115@gmail.com>
2024-02-25 12:57:50 +0200

    add album
    1

--- src/components/user.js
+++ src/components/user.js
@@ -1,7 +1,8 @@
 import { getData } from "../api";
-import { markupByUserId } from "../markup";
-import { userInfoTableEl } from "../refs";
+import { markupByUserId, createListAlbum } from "../markup";
+import { userInfoTableEl, userInfoAlbumEl } from "../refs";
 import { addMarkup } from "../helpers";
+
 const search = location.search;
 const searchParams = new URLSearchParams(search);
 const paramsId = searchParams.get("userid");
@@ -14,3 +15,10 @@ getData(`users/${paramsId}`)
   .catch((error) => {
     console.log(error);
   });
+
+getData(`albums?userId=${paramsId}`)
+  .then((res) => {
+    const markup = createListAlbum(res);
+    addMarkup(userInfoAlbumEl, markup);
+  })
+  .catch((error) => console.log(error));
