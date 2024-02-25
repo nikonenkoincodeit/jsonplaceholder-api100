28d00c50bf5818a0b1557f489235e4749d30b435
VitaPad <v.mars22@ukr.net>
2024-02-25 11:51:30 +0200

    a

--- src/api/index.js
+++ src/api/index.js
@@ -7,3 +7,14 @@ axios.defaults.baseURL = BASE_URL;
 export function getData(data) {
   return axios.get(data).then((response) => response.data);
 }
+
+const BASE_URL = "https://jsonplaceholder.typicode.com/";
+axios.defaults.baseURL = BASE_URL;
+
+export function getData() {
+  return axios.get(data).then((response) => response.data);
+}
+
+export function addMarkup(link, markup) {
+  link.inerHTML = markup;
+}
